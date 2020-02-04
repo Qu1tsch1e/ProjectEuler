ea8ff47e2437e46bdf9a23fd68eec901348297d9
Christoph Wörz <quitschie11@gmx.de>
2020-02-04 23:12:34 +0100

    solved problem 15

---
+++ Problem15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // !(40) / ((20)! * (20)!)
+            BigInteger x1 = new BigInteger(39);
+            BigInteger x2 = new BigInteger(37);
+            BigInteger x3 = new BigInteger(35);
+            BigInteger x4 = new BigInteger(33);
+            BigInteger x5 = new BigInteger(31);
+            BigInteger x6 = new BigInteger(29);
+            BigInteger x7 = new BigInteger(23);
+            BigInteger x8 = new BigInteger(4);
+            BigInteger calc = x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8;
+            Console.WriteLine(calc);
+            Console.ReadLine();
+        }
+    }
+}
